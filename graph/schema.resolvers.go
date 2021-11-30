cedc46a8d5c9b2627e66897770e645ca5b16dd65
yuki5664 <monkey.yebisu.7777@gmail.com>
2021-11-30 20:28:53 +0900

    imprements resolver

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -8,14 +8,40 @@ import (
 	"fmt"
 	"graphql_golang/graph/generated"
 	"graphql_golang/graph/model"
+	"strconv"
 )
 
 func (r *mutationResolver) UpsertCharacter(ctx context.Context, input model.CharacterInput) (*model.Character, error) {
-	panic(fmt.Errorf("not implemented"))
+	id := input.ID
+	var character model.Character
+	character.Name = input.Name
+
+	n := len(r.Resolver.CharacterStore)
+	if n == 0 {
+		r.Resolver.CharacterStore = make(map[string]model.Character)
+	}
+
+	if id != nil {
+		_, ok := r.Resolver.CharacterStore[*id]
+		if !ok {
+			return nil, fmt.Errorf("not found")
+		}
+		r.Resolver.CharacterStore[*id] = character
+	} else {
+		// generate unique id
+		nid := strconv.Itoa(n + 1)
+		character.ID = nid
+		r.Resolver.CharacterStore[nid] = character
+	}
+	return &character, nil
 }
 
 func (r *queryResolver) Character(ctx context.Context, id string) (*model.Character, error) {
-	panic(fmt.Errorf("not implemented"))
+	character, ok := r.Resolver.CharacterStore[id]
+	if !ok {
+		return nil, fmt.Errorf("not found")
+	}
+	return &character, nil
 }
 
 func (r *queryResolver) Pogues(ctx context.Context) ([]*model.Character, error) {
@@ -34,19 +60,3 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *mutationResolver) UpserCharacter(ctx context.Context, input model.CharacterInput) (*model.Character, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
-}
